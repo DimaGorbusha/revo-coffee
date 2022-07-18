6edf5d121e1fc112fa0eb82a5d2cadaedfda4ef8
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-07-19 01:01:34 +0500

    carousel items template ready

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import coffe1 from './coffe1'
+import coffe1 from './coffe1.png'
 
 export default function Carousel() {
   return (
@@ -8,11 +8,61 @@ export default function Carousel() {
             <h2 className="text-[20px] font-nunito-semibold text-main items-right">Choose Your Favorite</h2>
             <h1 className="text-[30px] font-mon-black text-main items-center">CHUẨN GU ĐÚNG VỊ</h1>
         </div>
-        <div className='w-[800px] m-auto'>
-          <div className='flex overflow-x-auto'>
-            <div className='w-[559px] h-[256px] flex'>
-              <img src="" alt="" />
+        <div className='w-auto m-auto mb-[100px]'>
+          <div className='flex overflow-x-auto mt-[72px]'>
+
+            <div className='flex rounded-[10px] mr-[29px] min-h-full min-w-[50%]'>
+              <img src={coffe1} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
+              <div className='ml-[44px] mt-[42px]'>
+                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>99.000</h2>
+                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Morning</h1>
+                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa </p>
+                <div className='flex text-center align-middle'>
+                  <a href="#" className=' font-nunito-bold text-[#F9FBFF] text-[16px] pr-[25px] pl-[25px] pt-[6px] pb-[6px] bg-accent_dark rounded-[19px] transition delay-75 ease-in-out hover:text-accent_dark hover:bg-[#ECEFF4] '>MUA NGAY</a>
+                  <p className='font-nunito-bold text-[16px] text-main mt-[6px] ml-[29px]'>CHI TIẾT</p>
+                </div>
+              </div>
             </div>
+
+            <div className='flex rounded-[10px] mr-[29px] min-h-full min-w-[50%]'>
+              <img src={coffe1} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
+              <div className='ml-[44px] mt-[42px]'>
+                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>99.000</h2>
+                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Morning</h1>
+                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa </p>
+                <div className='flex text-center align-middle'>
+                  <a href="#" className=' font-nunito-bold text-[#F9FBFF] text-[16px] pr-[25px] pl-[25px] pt-[6px] pb-[6px] bg-accent_dark rounded-[19px] transition delay-75 ease-in-out hover:text-accent_dark hover:bg-[#ECEFF4] '>MUA NGAY</a>
+                  <p className='font-nunito-bold text-[16px] text-main mt-[6px] ml-[29px]'>CHI TIẾT</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='flex rounded-[10px] mr-[29px] min-h-full min-w-[50%]'>
+              <img src={coffe1} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
+              <div className='ml-[44px] mt-[42px]'>
+                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>99.000</h2>
+                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Morning</h1>
+                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa </p>
+                <div className='flex text-center align-middle'>
+                  <a href="#" className=' font-nunito-bold text-[#F9FBFF] text-[16px] pr-[25px] pl-[25px] pt-[6px] pb-[6px] bg-accent_dark rounded-[19px] transition delay-75 ease-in-out hover:text-accent_dark hover:bg-[#ECEFF4] '>MUA NGAY</a>
+                  <p className='font-nunito-bold text-[16px] text-main mt-[6px] ml-[29px]'>CHI TIẾT</p>
+                </div>
+              </div>
+            </div>
+
+            <div className='flex rounded-[10px] mr-[29px] min-h-full min-w-[50%]'>
+              <img src={coffe1} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
+              <div className='ml-[44px] mt-[42px]'>
+                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>99.000</h2>
+                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Morning</h1>
+                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa </p>
+                <div className='flex text-center align-middle'>
+                  <a href="#" className=' font-nunito-bold text-[#F9FBFF] text-[16px] pr-[25px] pl-[25px] pt-[6px] pb-[6px] bg-accent_dark rounded-[19px] transition delay-75 ease-in-out hover:text-accent_dark hover:bg-[#ECEFF4] '>MUA NGAY</a>
+                  <p className='font-nunito-bold text-[16px] text-main mt-[6px] ml-[29px]'>CHI TIẾT</p>
+                </div>
+              </div>
+            </div>
+
           </div>
         </div>
       </div>
