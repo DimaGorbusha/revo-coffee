904fb3087060e7192db18116b243d3e7233e575f
Dmitriy Gorbunov <dmitrgor1@yandex.ru>
2022-08-08 17:31:28 +0300

    сделано наполнение карточек

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
-import coffe1 from './img/coffe1.png'
+import coffee_morning from './img/coffee_morning.png'
+import coffee_origin from './img/coffee_origin.png'
+import coffee_everyday from './img/coffee_everyday.png'
+import coffee_dam_da from './img/coffee_dam_da.png'
 
 export default function Carousel() {
   return (
@@ -12,11 +15,11 @@ export default function Carousel() {
           <div className='flex overflow-x-auto mt-[72px] snap-x snap-mandatory'>
 
             <div className='flex rounded-[10px] mr-[29px] min-h-full min-w-[50%] ml-[149px]'>
-              <img src={coffe1} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
+              <img src={coffee_morning} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
               <div className='ml-[44px] mt-[42px]'>
                 <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>99.000</h2>
                 <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Morning</h1>
-                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa </p>
+                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa</p>
                 <div className='flex text-center align-middle'>
                   <a href="#" className=' font-nunito-bold text-[#F9FBFF] text-[16px] pr-[25px] pl-[25px] pt-[6px] pb-[6px] bg-accent_dark rounded-[19px] transition delay-75 ease-in-out hover:text-accent_dark hover:bg-[#ECEFF4] '>MUA NGAY</a>
                   <p className='font-nunito-bold text-[16px] text-main mt-[6px] ml-[29px]'>CHI TIẾT</p>
@@ -25,11 +28,11 @@ export default function Carousel() {
             </div>
 
             <div className='flex rounded-[10px] mr-[29px] min-h-full min-w-[50%]'>
-              <img src={coffe1} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
+              <img src={coffee_origin} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
               <div className='ml-[44px] mt-[42px]'>
-                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>99.000</h2>
-                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Morning</h1>
-                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa </p>
+                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>139.000</h2>
+                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Origin</h1>
+                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>hậu ngọt, ít đắng, vị chua dâu</p>
                 <div className='flex text-center align-middle'>
                   <a href="#" className=' font-nunito-bold text-[#F9FBFF] text-[16px] pr-[25px] pl-[25px] pt-[6px] pb-[6px] bg-accent_dark rounded-[19px] transition delay-75 ease-in-out hover:text-accent_dark hover:bg-[#ECEFF4] '>MUA NGAY</a>
                   <p className='font-nunito-bold text-[16px] text-main mt-[6px] ml-[29px]'>CHI TIẾT</p>
@@ -38,11 +41,11 @@ export default function Carousel() {
             </div>
 
             <div className='flex rounded-[10px] mr-[29px] min-h-full min-w-[50%]'>
-              <img src={coffe1} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
+              <img src={coffee_everyday} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
               <div className='ml-[44px] mt-[42px]'>
-                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>99.000</h2>
-                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Morning</h1>
-                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa </p>
+                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>85.000</h2>
+                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Everyday</h1>
+                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>cân bằng, hậu đắng, vị chocolate</p>
                 <div className='flex text-center align-middle'>
                   <a href="#" className=' font-nunito-bold text-[#F9FBFF] text-[16px] pr-[25px] pl-[25px] pt-[6px] pb-[6px] bg-accent_dark rounded-[19px] transition delay-75 ease-in-out hover:text-accent_dark hover:bg-[#ECEFF4] '>MUA NGAY</a>
                   <p className='font-nunito-bold text-[16px] text-main mt-[6px] ml-[29px]'>CHI TIẾT</p>
@@ -51,11 +54,11 @@ export default function Carousel() {
             </div>
 
             <div className='flex rounded-[10px] mr-[29px] min-h-full min-w-[50%]'>
-              <img src={coffe1} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
+              <img src={coffee_dam_da} alt="" className='w-[235px] h-[256px] rounded-[10px]'/>
               <div className='ml-[44px] mt-[42px]'>
-                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>99.000</h2>
-                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Morning</h1>
-                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đắng, hậu ngọt, hương hoa </p>
+                <h2 className='font-nunito-semibold text-accent_dark text-[20px]'>75.000</h2>
+                <h1 className='font-nunito-bold text-[#232C38] text-[24px] mb-[24px]'>REVO Đậm Đà</h1>
+                <p className='font-nunito-light text-dark_gray text-[16px] mb-[22px]'>đậm đà, đắng, truyền thống</p>
                 <div className='flex text-center align-middle'>
                   <a href="#" className=' font-nunito-bold text-[#F9FBFF] text-[16px] pr-[25px] pl-[25px] pt-[6px] pb-[6px] bg-accent_dark rounded-[19px] transition delay-75 ease-in-out hover:text-accent_dark hover:bg-[#ECEFF4] '>MUA NGAY</a>
                   <p className='font-nunito-bold text-[16px] text-main mt-[6px] ml-[29px]'>CHI TIẾT</p>
